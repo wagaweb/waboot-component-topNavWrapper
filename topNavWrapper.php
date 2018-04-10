7e7b0eea10160efa69bf536d3be088cfc3714e62
LostCore <lostcore@gmail.com>
2018-04-10 17:27:32 +0200

    Standardized version

--- topNavWrapper.php
+++ topNavWrapper.php
@@ -4,7 +4,7 @@ Component Name: Top Nav Wrapper
 Description: Top Nav Wrapper Component
 Category: Layout
 Tags: Navigation
-Version: 1.0
+Version: 1.0.0
 Author: WAGA Team <dev@waga.it>
 Author URI: http://www.waga.it
  */
