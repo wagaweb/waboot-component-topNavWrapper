32ecaf2fd934969b6e44826db1c9d9a35751e2e7
Riccardo D'Angelo <riccardo@waga.it>
2018-04-23 15:40:00 +0200

    Headers updates

--- topNavWrapper.php
+++ topNavWrapper.php
@@ -5,8 +5,8 @@ Description: Top Nav Wrapper Component
 Category: Layout
 Tags: Navigation
 Version: 1.0.0
-Author: WAGA Team <dev@waga.it>
-Author URI: http://www.waga.it
+Author: Waboot Team <info@waboot.io>
+Author URI: http://www.waboot.io
  */
 
 if(!class_exists("\\Waboot\\Component")){
